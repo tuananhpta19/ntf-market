969f6dfbc8c2de33c216b63ae483fc383578dde5
tuananhpta19 <tuananh.pta19@gmail.com>
2021-09-14 00:59:10 +0700

    respon apply và thay doi id nut next slide

--- vendor/js/index.js
+++ vendor/js/index.js
@@ -2,42 +2,70 @@ var carouselMarket = $('.owl-carousel-market');
 
 carouselMarket.owlCarousel({
     // center: true,
-    loop:true,
-    margin:9,
-    autoWidth:true,
-    items:4
-    // responsive:{
-    //     600:{
-    //         items:4
-    //     }
-    // }
+    loop: true,
+    margin: 9,
+    autoWidth: true,
+    // items:4
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 4
+        }
+    }
 })
-
-$(".btn-next").click(function() {
+$("#btn-next__market").click(function () {
     carouselMarket.trigger('next.owl.carousel');
 });
 
-$(".btn-prev").click(function() {
+$("#btn-prev__market").click(function () {
     carouselMarket.trigger('prev.owl.carousel');
 });
 
 var carouselEconomics = $('.owl-carousel-economics');
 
-carouselEconomics.owlCarousel({
-    loop:true,
-    margin:10,
-    responsive:{
-        600:{
-            items:3
+if (parseInt($(window).width()) < 992) {
+    carouselEconomics.owlCarousel({
+        loop: true,
+        margin: 9,
+        autoWidth: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            540: {
+                items: 2
+            },
+            720: {
+                item: 3
+            }
         }
-    }
-})
+    })
+}else{
+    carouselEconomics.owlCarousel({
+        margin: 9,
+        autoWidth: true,
+        responsive:{
+            0:{
+                items: 1
+            },
+            540:{
+                items: 2
+            },
+            720:{
+                item: 3
+            }
+        }
+    })
+}
 
-$(".btn-next").click(function() {
+
+$("#btn-next__economics").click(function () {
     carouselEconomics.trigger('next.owl.carousel');
 });
 
-$(".btn-prev").click(function() {
+$("#btn-prev__economics").click(function () {
     carouselEconomics.trigger('prev.owl.carousel');
 });
 var carouselSquare = $('.owl-carousel-square');
@@ -46,21 +74,17 @@ carouselSquare.owlCarousel({
     // loop:true,
     // margin:10,
     // autoWidth: true,
-    responsive:{
-        0:{
-            items:1
-        }
+    responsive: {
+        0: {
+            items: 1
+        },
     }
 })
 
-$(".btn-next").click(function() {
+$("#btn-next__nft__square").click(function () {
     carouselSquare.trigger('next.owl.carousel');
 });
 
-$(".btn-prev").click(function() {
+$("#btn-prev__nft__square").click(function () {
     carouselSquare.trigger('prev.owl.carousel');
 });
-
-$(document).ready(function(){
-
-})
